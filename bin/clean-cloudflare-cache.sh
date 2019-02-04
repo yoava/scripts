e02b2aad69162098fa7216d768b63cb20e14d541
Yoav Aharoni <yoavah@gmail.com>
2019-02-04 02:19:56 +0200

    update scripts
    update scripts

--- bin/clean-cloudflare-cache.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-echo Clean Cloudflare cache...
-curl -X DELETE https://api.cloudflare.com/client/v4/zones/$CLOUDFLARE_ZONE_ID/purge_cache \
-  -H "X-Auth-Email: $CLOUDFLARE_EMAIL" \
-  -H "X-Auth-Key: $CLOUDFLARE_KEY" \
-  -H "Content-Type: application/json" \
-  --data '{"purge_everything":true}'
